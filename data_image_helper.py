57bf4016482961d66ec2f1b52047301e983777bf
JZX <505684228@qq.com>
2019-04-25 20:38:45 +0800

    a

--- data_image_helper.py
+++ data_image_helper.py
@@ -31,6 +31,9 @@ class data_image_helper:
         cnt = 0
         frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
         fps = cap.get(cv2.CAP_PROP_FPS)
+        frames = 1000
+        fps = 25
+
         v_length = frames / fps
         if (end == 0 or end >= v_length):
             end = v_length
