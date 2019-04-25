b069d9de3f2a0cf6ef17255ae31f59b7a2ed1879
JZX <505684228@qq.com>
2019-04-25 20:24:23 +0800

    a

--- data_image_helper.py
+++ data_image_helper.py
@@ -29,6 +29,8 @@ class data_image_helper:
         images = []
         mouth = None
         cnt = 0
+        print(cap)
+        print(path)
         frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
         fps = cap.get(cv2.CAP_PROP_FPS)
         v_length = frames / fps
